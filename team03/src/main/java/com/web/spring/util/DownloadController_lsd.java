acd620d9148dd741788b26d1c38cf1fcb6592bab
user <user@DESKTOP-COJ9858>
2024-01-31 14:24:06 +0900

    downloadcontroller

---
+++ team03/src/main/java/com/web/spring/util/DownloadController_lsd.java
@@ -0,0 +1,16 @@
+package com.web.spring.util;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class DownloadController_lsd {
+	
+	@RequestMapping("downloadNotice")
+	public String download(@RequestParam("fname") String fname, Model d) {
+		d.addAttribute("downloadFile", fname);
+		return "downloadViewer_lsd";
+	}
+}
