98717efb0b3c393491c9b0757e7856fe3a793265
user <user@DESKTOP-9CU9TCV>
2024-02-14 12:51:32 +0900

    Merge branch 'master' of https://github.com/chungjea/NPpms.git

---
+++ team03/src/main/java/com/web/spring/vo/FileSch3.java
@@ -0,0 +1,152 @@
+package com.web.spring.vo;
+
+public class FileSch3 {
+	private int empno;
+	private int deptno;
+	private int wempno;
+	private int cempno;
+	private int manager;
+	private int count3;
+	private int pageSize3;
+	private int pageCount3;
+	private int curPage3;
+	private int start3;
+	private int end3;
+	private int blockSize3;
+	private int startBlock3;
+	private int endBlock3;
+	
+	public FileSch3() {
+	}
+
+	public FileSch3(int empno, int deptno, int wempno, int cempno, int manager, int count3, int pageSize3,
+			int pageCount3, int curPage3, int start3, int end3, int blockSize3, int startBlock3, int endBlock3) {
+		this.empno = empno;
+		this.deptno = deptno;
+		this.wempno = wempno;
+		this.cempno = cempno;
+		this.manager = manager;
+		this.count3 = count3;
+		this.pageSize3 = pageSize3;
+		this.pageCount3 = pageCount3;
+		this.curPage3 = curPage3;
+		this.start3 = start3;
+		this.end3 = end3;
+		this.blockSize3 = blockSize3;
+		this.startBlock3 = startBlock3;
+		this.endBlock3 = endBlock3;
+	}
+
+	public int getEmpno() {
+		return empno;
+	}
+
+	public void setEmpno(int empno) {
+		this.empno = empno;
+	}
+
+	public int getDeptno() {
+		return deptno;
+	}
+
+	public void setDeptno(int deptno) {
+		this.deptno = deptno;
+	}
+
+	public int getWempno() {
+		return wempno;
+	}
+
+	public void setWempno(int wempno) {
+		this.wempno = wempno;
+	}
+
+	public int getCempno() {
+		return cempno;
+	}
+
+	public void setCempno(int cempno) {
+		this.cempno = cempno;
+	}
+
+	public int getManager() {
+		return manager;
+	}
+
+	public void setManager(int manager) {
+		this.manager = manager;
+	}
+
+	public int getCount3() {
+		return count3;
+	}
+
+	public void setCount3(int count3) {
+		this.count3 = count3;
+	}
+
+	public int getPageSize3() {
+		return pageSize3;
+	}
+
+	public void setPageSize3(int pageSize3) {
+		this.pageSize3 = pageSize3;
+	}
+
+	public int getPageCount3() {
+		return pageCount3;
+	}
+
+	public void setPageCount3(int pageCount3) {
+		this.pageCount3 = pageCount3;
+	}
+
+	public int getCurPage3() {
+		return curPage3;
+	}
+
+	public void setCurPage3(int curPage3) {
+		this.curPage3 = curPage3;
+	}
+
+	public int getStart3() {
+		return start3;
+	}
+
+	public void setStart3(int start3) {
+		this.start3 = start3;
+	}
+
+	public int getEnd3() {
+		return end3;
+	}
+
+	public void setEnd3(int end3) {
+		this.end3 = end3;
+	}
+
+	public int getBlockSize3() {
+		return blockSize3;
+	}
+
+	public void setBlockSize3(int blockSize3) {
+		this.blockSize3 = blockSize3;
+	}
+
+	public int getStartBlock3() {
+		return startBlock3;
+	}
+
+	public void setStartBlock3(int startBlock3) {
+		this.startBlock3 = startBlock3;
+	}
+
+	public int getEndBlock3() {
+		return endBlock3;
+	}
+
+	public void setEndBlock3(int endBlock3) {
+		this.endBlock3 = endBlock3;
+	}
+
+}
